2e3aab563b6bcf79dceaed57666cf6eccb81992d
MGutix <140160200+MGutix@users.noreply.github.com>
2023-08-30 15:19:52 +0900

    Completed Fibonacci all tests Passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    if (x < 1){
+        return 'OOPS';
+    }
 
+    let arr = [0,1];
+
+    for (let i = 2; i <= x; i++) {
+        arr.push(arr[i-2]+arr[i-1]); 
+    }
+
+    return arr[x];
 };
 
 // Do not edit below this line
