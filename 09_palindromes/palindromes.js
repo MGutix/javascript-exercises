85f78b6e7dcf1d8cbf2111ac19b8020d273c123b
MGutix <140160200+MGutix@users.noreply.github.com>
2023-08-30 14:22:05 +0900

    Completed Palindromes with all test passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let lower = text.toLowerCase();
+    let pattern = /([a-z0-9])/g
+    let result = lower.match(pattern);
+    let len = result.length
+    let half = Math.floor(result.length / 2)
 
+    if (result.length % 2 == 0) { 
+        half = result.length / 2
+    };
+
+    for (let i = 0; i <= half; i++) {
+        if (result[i] != result[len- 1 - i]){
+            return false;
+        }
+        
+        
+    }
+    return true;
 };
 
 // Do not edit below this line
