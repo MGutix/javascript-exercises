55be3732fcd9610b890f852f87a40ce53ca3ff30
MGutix <140160200+MGutix@users.noreply.github.com>
2023-08-30 15:51:30 +0900

    Completed findTheOldest all tests Passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    let max = obj[0];
+    obj.forEach(person => {
+        if (person.hasOwnProperty('yearOfDeath') === false){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    });
+    for (let i = 1; i < obj.length; i++) {
+        if ((obj[i].yearOfDeath-obj[i].yearOfBirth) > (max.yearOfDeath-max.yearOfBirth)) {
+            max = obj[i];
+        }
+        
+    }
+    return max;
 };
 
 // Do not edit below this line
